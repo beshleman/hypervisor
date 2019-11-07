1723d4ae5c459c055372b73fa197fc24ea365ba3
Bobby Eshleman <bobbyeshleman@gmail.com>
2019-11-07 08:22:30 -0600

    change qemu scripts to target the cortex-a53 instead of the a57

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aarch64-linux-gnu-gdb target/aarch64-unknown-linux-gnu/debug/hypervisor.elf -ex "target remote localhost:1234" -ex "hb start_mythril" -ex "c"
+aarch64-linux-gnu-gdb target/aarch64-unknown-linux-gnu/debug/hypervisor.elf -ex "target remote localhost:1234" -ex "hb start_mythril" -ex "c" -ex "layout src"
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 sudo cp target/aarch64-unknown-linux-gnu/debug/hypervisor /mnt/hypervisor.elf
 
@@ -9,12 +10,13 @@ killall -q -s 9 $(which qemu-system-aarch64)
 set -e
 
 #	-machine virt,gic_version=3 \
-#qemu-system-aarch64 -M virt -cpu cortex-a57 -nographic -smp 1 -bios ../u-boot/u-boot.bin \
+#qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -smp 1 -bios ../u-boot/u-boot.bin \
 #    -machine virtualization=true	\
 #    -drive if=none,id=hd0,file=../disk.img \
 #    -device virtio-blk-device,drive=hd0 -S -s
 
-qemu-system-aarch64 -M virt -cpu cortex-a57 -nographic -smp 1 -kernel target/aarch64-unknown-linux-gnu/debug/hypervisor.bin \
+qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -smp 1 \
+    -kernel /home/bobbye/projects/hypervisor/target/aarch64-unknown-linux-gnu/debug/hypervisor.bin	\
     -machine virtualization=true	\
-    -drive if=none,id=hd0,file=../disk.img \
+    -drive if=none,id=hd0,file=/home/bobbye/projects/disk.img \
     -device virtio-blk-device,drive=hd0 -S -s
